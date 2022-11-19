22c9d0161b05c91447a1288cbd7410523f1bfb1f
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 17:23:40 -0500

    definicion completa clase Reserva

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Reserva.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Reserva.java
@@ -4,10 +4,61 @@
  */
 package Sistema;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author HP
  */
 public class Reserva {
+    private String codigo;
+    private ArrayList<VueloReserva> vuelosReserva;
+    private Cliente cliente;
+    private Date fecha;
+    private double valorPagar;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public ArrayList<VueloReserva> getVuelosReserva() {
+        return vuelosReserva;
+    }
+
+    public void setVuelosReserva(ArrayList<VueloReserva> vuelosReserva) {
+        this.vuelosReserva = vuelosReserva;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getValorPagar() {
+        return valorPagar;
+    }
+
+    public void setValorPagar(double valorPagar) {
+        this.valorPagar = valorPagar;
+    }
     
+    private void crearCadena(){
+        
+    }
 }
