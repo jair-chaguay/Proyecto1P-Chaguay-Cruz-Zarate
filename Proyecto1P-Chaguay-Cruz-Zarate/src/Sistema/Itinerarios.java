2dbb4741776054a2b53680630d35c89fa72a582e
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 18:50:49 -0500

    update itinerarios

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Itinerarios.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Itinerarios.java
@@ -9,5 +9,56 @@ package Sistema;
  * @author HP
  */
 public class Itinerarios {
+    private int cod;
+    private String origenCiudad, destinoCiudad,duracion,horaSalida,horaLlegada;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getOrigenCiudad() {
+        return origenCiudad;
+    }
+
+    public void setOrigenCiudad(String origenCiudad) {
+        this.origenCiudad = origenCiudad;
+    }
+
+    public String getDestinoCiudad() {
+        return destinoCiudad;
+    }
+
+    public void setDestinoCiudad(String destinoCiudad) {
+        this.destinoCiudad = destinoCiudad;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setHoraSalida(String horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+
+    public String getHoraLlegada() {
+        return horaLlegada;
+    }
+
+    public void setHoraLlegada(String horaLlegada) {
+        this.horaLlegada = horaLlegada;
+    }
+    
     
 }
