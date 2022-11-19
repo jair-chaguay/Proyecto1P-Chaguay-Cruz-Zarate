6cce28e6ad6d5e3eb8bd88827e9562f7605e6e07
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 17:13:50 -0500

    definicion completa clase ClienteVip

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/ClienteVip.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/ClienteVip.java
@@ -8,6 +8,40 @@ package Sistema;
  *
  * @author HP
  */
-public class ClienteVip {
-    
+public class ClienteVip extends Cliente{
+    private tipoRango rango;
+    private int millas=1000;
+
+    public tipoRango getRango() {
+        return rango;
+    }
+
+    public void setRango(tipoRango rango) {
+        this.rango = rango;
+    }
+
+    public int getMillas() {
+        return millas;
+    }
+
+    public void setMillas(int millas) {
+        this.millas = millas;
+    }
+    @Override
+    public void comprarTickets(){
+        
+    }
+    @Override
+    public void consultarReservas(){
+        
+    }
+    @Override
+    public void elegirOpcion(){
+        
+    }
+    @Override
+    public void pagar(int millas){
+        
+    }
+   
 }
