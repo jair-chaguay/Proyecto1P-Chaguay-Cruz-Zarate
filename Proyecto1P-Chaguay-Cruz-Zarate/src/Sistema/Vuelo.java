ad384a6eddf1ca5df5e0f103c8c57241f1ac1ca2
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 18:18:06 -0500

    update Vuelo

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Vuelo.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Vuelo.java
@@ -4,10 +4,76 @@
  */
 package Sistema;
 
+import java.util.Date;
+
 /**
  *
  * @author HP
  */
 public class Vuelo {
+    private Avion avion;
+    private double precio;
+    private Itinerarios itinerario;
+    private Date fechaSalida;
+    private Date fechaLlegada;
+    private int millas;
+    private String codigoVuelo;
+
+    public Avion getAvion() {
+        return avion;
+    }
+
+    public void setAvion(Avion avion) {
+        this.avion = avion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Itinerarios getItinerario() {
+        return itinerario;
+    }
+
+    public void setItinerario(Itinerarios itinerario) {
+        this.itinerario = itinerario;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Date getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public void setFechaLlegada(Date fechaLlegada) {
+        this.fechaLlegada = fechaLlegada;
+    }
+
+    public int getMillas() {
+        return millas;
+    }
+
+    public void setMillas(int millas) {
+        this.millas = millas;
+    }
+
+    public String getCodigoVuelo() {
+        return codigoVuelo;
+    }
+
+    public void setCodigoVuelo(String codigoVuelo) {
+        this.codigoVuelo = codigoVuelo;
+    }
+    
     
 }
