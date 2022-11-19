022d510eeb4858c5061e83bc9ea533c62737cdcf
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 18:19:45 -0500

    update Asientos

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Avion.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Avion.java
@@ -4,10 +4,42 @@
  */
 package Sistema;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author HP
  */
 public class Avion {
+    private String codigoAvion;
+    private int capacidad;
+    private ArrayList<Asientos> listaAsientos;
+
+    public String getCodigoAvion() {
+        return codigoAvion;
+    }
+
+    public void setCodigoAvion(String codigoAvion) {
+        this.codigoAvion = codigoAvion;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public ArrayList<Asientos> getListaAsientos() {
+        return listaAsientos;
+    }
+
+    public void setListaAsientos(ArrayList<Asientos> listaAsientos) {
+        this.listaAsientos = listaAsientos;
+    }
     
+    public void asignarAsiento(){
+        
+    }
 }
