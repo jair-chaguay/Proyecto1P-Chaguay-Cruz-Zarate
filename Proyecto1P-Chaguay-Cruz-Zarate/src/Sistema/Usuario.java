7f3347c408bcb583575f57989ce0126c3da7ae80
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 16:59:09 -0500

    definicion completa clase Usuario

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Usuario.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Usuario.java
@@ -8,6 +8,75 @@ package Sistema;
  *
  * @author HP
  */
-public class Usuario {
+public abstract class Usuario {
+    protected String cedula;
+    protected String nombres;
+    protected String apellidos;
+    protected int edad;
+    protected String correo;
+    protected String contrasena;
+    protected tipoCategoria tipoCategoria;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public tipoCategoria getTipoCategoria() {
+        return tipoCategoria;
+    }
+
+    public void setTipoCategoria(tipoCategoria tipoCategoria) {
+        this.tipoCategoria = tipoCategoria;
+    }
+    
+    public abstract void consultarReservas();
+    
+    public void elegirOpcion(){
+        
+    }
     
 }
