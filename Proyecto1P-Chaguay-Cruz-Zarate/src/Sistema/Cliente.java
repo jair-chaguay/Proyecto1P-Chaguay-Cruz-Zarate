35a7205c5e5c17e6b4894ceb6f58e1af3d5550cc
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 17:04:04 -0500

    definicion completa clase Cliente

---
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Cliente.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sistema;
+
+/**
+ *
+ * @author HP
+ */
+public class Cliente extends Usuario {
+    private int numTarjetaCredito;
+
+    public int getNumTarjetaCredito() {
+        return numTarjetaCredito;
+    }
+
+    public void setNumTarjetaCredito(int numTarjetaCredito) {
+        this.numTarjetaCredito = numTarjetaCredito;
+    }
+   
+    @Override
+    public void consultarReservas(){
+        
+    }
+    public void comprarTickets(){
+        
+    }
+    @Override
+    public void elegirOpcion(){
+        
+    }
+    public void pagar(int numTarjetaCredito){
+        
+    }
+    public void crearReserva(){
+        
+    }
+}
