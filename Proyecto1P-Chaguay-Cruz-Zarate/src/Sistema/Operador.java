60d5300a461cbe2be06a4dd823bcb449ba4b6be4
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 17:18:45 -0500

    definicion completa clase Operario

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Operador.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Operador.java
@@ -8,6 +8,23 @@ package Sistema;
  *
  * @author HP
  */
-public class Operador {
+public class Operador extends Usuario{
+    private double sueldo;
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    @Override
+    public void consultarReservas(){
+        
+    }
+    @Override
+    public void elegirOpcion(){
+        
+    }
     
 }
