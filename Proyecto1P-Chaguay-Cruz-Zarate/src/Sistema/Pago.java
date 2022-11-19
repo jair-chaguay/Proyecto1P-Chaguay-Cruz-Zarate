3df39f7639e0f9bf6e8290947570e95437e5967a
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-19 17:27:30 -0500

    definicion completa clase Pago

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Pago.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Pago.java
@@ -4,10 +4,60 @@
  */
 package Sistema;
 
+import java.util.Date;
+
 /**
  *
  * @author HP
  */
 public class Pago {
+    private Date fechaPago;
+    private int idPago;
+    private Reserva reserva;
+    private formaPago formaPago;
+    private double valorPagar;
+
+    public Date getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(Date fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public int getIdPago() {
+        return idPago;
+    }
+
+    public void setIdPago(int idPago) {
+        this.idPago = idPago;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
+
+    public formaPago getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(formaPago formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public double getValorPagar() {
+        return valorPagar;
+    }
+
+    public void setValorPagar(double valorPagar) {
+        this.valorPagar = valorPagar;
+    }
     
+    public void crearIdPago(){
+        
+    }
 }
