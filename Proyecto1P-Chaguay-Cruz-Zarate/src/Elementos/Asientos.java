b2c27cc1f0f80e812f437f557ffcae7e19d1871e
angmcruz <Userx@192.168.1.7>
2022-11-19 22:59:51 -0500

    actualizar

---
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Elementos/Asientos.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Elementos;
+
+import Enums.disponibilidad;
+import Sistema.*;
+import Enums.*;
+
+/**
+ *
+ * @author HP
+ */
+public class Asientos {
+    private Avion avion;
+    private String numAsiento;
+    private disponibilidad disponible;
+
+    public Avion getAvion() {
+        return avion;
+    }
+
+    public void setAvion(Avion avion) {
+        this.avion = avion;
+    }
+
+    public String getNumAsiento() {
+        return numAsiento;
+    }
+
+    public void setNumAsiento(String numAsiento) {
+        this.numAsiento = numAsiento;
+    }
+
+    public disponibilidad getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(disponibilidad disponible) {
+        this.disponible = disponible;
+    }
+    
+    
+}
