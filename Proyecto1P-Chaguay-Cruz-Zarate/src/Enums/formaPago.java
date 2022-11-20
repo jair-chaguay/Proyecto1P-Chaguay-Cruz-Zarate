c237945c52f100f66857f9b30e57285470de1a69
angmcruz <Userx@192.168.1.7>
2022-11-19 23:03:19 -0500

    actualizar

---
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Enums/formaPago.java
@@ -0,0 +1,17 @@
+package Enums;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+
+/**
+ *
+ * @author DELL
+ */
+public enum formaPago {
+    TC,
+    M
+}
