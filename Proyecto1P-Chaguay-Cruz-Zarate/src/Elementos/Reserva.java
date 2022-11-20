93d36e3fb28eb2a4098c51c2260b6af0a5b18d39
angmcruz <Userx@192.168.1.7>
2022-11-19 23:00:23 -0500

    actualizar

---
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Elementos/Reserva.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Elementos;
+
+import Sistema.*;
+import Enums.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author HP
+ */
+public class Reserva {
+    private String codigo;
+    private ArrayList<VueloReserva> vuelosReserva=new ArrayList<>();
+    private Cliente cliente;
+    private Date fecha;
+    private double valorPagar;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public ArrayList<VueloReserva> getVuelosReserva() {
+        return vuelosReserva;
+    }
+
+    public void setVuelosReserva(ArrayList<VueloReserva> vuelosReserva) {
+        this.vuelosReserva = vuelosReserva;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getValorPagar() {
+        return valorPagar;
+    }
+
+    public void setValorPagar(double valorPagar) {
+        this.valorPagar = valorPagar;
+    }
+    
+    private String crearCadena(){
+        
+        String opciones="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String caleatoria="";
+        for(int i=0; i<6;i++){
+            int 
+        }
+        return caleatoria;
+    }
+}
