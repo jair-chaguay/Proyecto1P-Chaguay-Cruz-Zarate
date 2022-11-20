ac40c8a95a0f01503acd315d7572059c27fb8e2d
angmcruz <Userx@192.168.1.7>
2022-11-19 23:00:41 -0500

    actualizar

---
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Elementos/VueloReserva.java
@@ -0,0 +1,65 @@
+package Elementos;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import Sistema.*;
+import Enums.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HP
+ */
+public class VueloReserva {
+    private Vuelo codigoVuelo;
+    private tipoVuelo tipoVuelo;
+    private tipoTarifa tarifa;
+    private String asiento;
+    private ArrayList<Vuelo> listaVuelos=new ArrayList<>();
+
+    public Vuelo getCodigoVuelo() {
+        return codigoVuelo;
+    }
+
+    public void setCodigoVuelo(Vuelo codigoVuelo) {
+        this.codigoVuelo = codigoVuelo;
+    }
+
+    public tipoVuelo getTipoVuelo() {
+        return tipoVuelo;
+    }
+
+    public void setTipoVuelo(tipoVuelo tipoVuelo) {
+        this.tipoVuelo = tipoVuelo;
+    }
+
+    public tipoTarifa getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(tipoTarifa tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(String asiento) {
+        this.asiento = asiento;
+    }
+
+    public ArrayList<Vuelo> getListaVuelos() {
+        return listaVuelos;
+    }
+
+    public void setListaVuelos(ArrayList<Vuelo> listaVuelos) {
+        this.listaVuelos = listaVuelos;
+    }
+    
+    
+}
