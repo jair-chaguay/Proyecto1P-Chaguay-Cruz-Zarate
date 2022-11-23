33d1c0416338c88eb6579764d2a19bbb848c388a
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-23 00:25:42 -0500

    actualizado main

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Sistema.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Sistema.java
@@ -19,22 +19,24 @@ public class Sistema {
    static ArrayList<Itinerarios> listaItinerarios;
    static ArrayList<Avion> listaAviones;
            
-   public void mostrarMenuCliente(){
-        System.out.println("1. Comprar tickets aereos");
-        System.out.println("2. Consultar reservas");
-        System.out.println("3. Salir");
-   }
-   public void mostrarMenuOpe(){
-       System.out.println("1. Consultar usuarios");
-       System.out.println("2. Consultar reservas");
-       System.out.println("3. Salir");
-   }
-   public void validarInformacion(){
-       
-   }
-   public void IniciarSesion(){
-       
-   }
+   
+
+//   public void validarInformacion(){
+//       
+//   }
+//   public void IniciarSesion(){
+//       
+//   }
+//    public void mostrarMenuOperario(){
+//        System.out.println("1. Consultar usuarios");
+//        System.out.println("2. Consultar reservas");
+//        System.out.println("3. Salir");
+//    }
+//    public void mostrarMenuCliente(){
+//        System.out.println("1. Comprar tickets aereos");
+//        System.out.println("2. Consultar reservas");
+//        System.out.println("3. Salir");
+//    }
    
     public static void main(String[] args){
         listaUsuarios=new ArrayList<>();
@@ -47,49 +49,54 @@ public class Sistema {
         System.out.println("++++++++++++++++++++++++++++++++++++++++");
         Scanner sc=new Scanner(System.in);
         System.out.print("USUARIO: ");
-        String usuario=sc.nextLine();
+        String user=sc.nextLine();
         System.out.print("CONTRASEÑA: ");
-        String contrasena=sc.nextLine();
-        
-        mostrarMenuCliente();
-        int opc=0;
-        while(opc!=3){
-            System.out.println("Ingrese opcion: ");
-            opc=sc.nextInt();
-            sc.nextLine();
-            switch(opc){
-                case 1:
-                   
-                   break;
-                case 2:
-                    
-                    break;
-                default:
-                    System.out.println("Opcion invalida");
-                    break;
+        String password=sc.nextLine();
+        for(Usuario usuario:listaUsuarios){
+            if(usuario.getUsuario().equals(user)){
+                if(usuario instanceof Cliente cliente){
+                    cliente.mostrarMenuCliente();
+                    int opc=0;
+                    while(opc!=3){
+                        System.out.println("Ingrese opcion: ");
+                        opc=sc.nextInt();
+                        sc.nextLine();
+                        switch(opc){
+                            case 1:
 
-            }
-        }
-        mostrarMenuOpe();
-        int opc2=0;
-        while(opc2!=3){
-            System.out.println("Ingrese opcion: ");
-            opc2=sc.nextInt();
-            sc.nextLine();
-            switch(opc2){
-                case 1:
-                   
-                   break;
-                case 2:
-                    
-                    break;
-                default:
-                    System.out.println("Opcion invalida");
-                    break;
+                               break;
+                            case 2:
 
-            }
+                                break;
+                            default:
+                                System.out.println("Opcion invalida");
+                                break;
+                        }
+                    }
+                }
+                if(usuario instanceof Operador operador){
+                    operador.mostrarMenuOperario();
+                    int opc2=0;
+                    while(opc2!=3){
+                        System.out.println("Ingrese opcion: ");
+                        opc2=sc.nextInt();
+                        sc.nextLine();
+                        switch(opc2){
+                            case 1:
+
+                               break;
+                            case 2:
+
+                                break;
+                            default:
+                                System.out.println("Opcion invalida");
+                                break;
+                        }
+                    }
+                }
+            }else{
+                System.out.println("Usuario o contraseña incorrectos");
+            }    
         }
-        
-   }
-   
+   } 
 }
