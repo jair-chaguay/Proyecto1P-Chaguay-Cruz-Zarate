27446ca2597b9dfeb7c56a6eb42fc50697fdb2ca
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-22 20:34:49 -0500

    actualizado

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Sistema.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Sistema.java
@@ -14,8 +14,11 @@ import java.util.Scanner;
  * @author HP
  */
 public class Sistema {
-   
-   
+   static ArrayList<Usuario> listaUsuarios;
+   static ArrayList<Reserva> listaReservas;
+   static ArrayList<Itinerarios> listaItinerarios;
+   static ArrayList<Avion> listaAviones;
+           
    public void mostrarMenuCliente(){
        
    }
@@ -29,22 +32,41 @@ public class Sistema {
        
    }
    
-   public static void main(String[] args){
-       ArrayList<Usuario> listaUsuarios=new ArrayList<>();
-       ArrayList<Reserva> listaReservas=new ArrayList<>();
-       ArrayList<Itinerarios> listaItinerarios=new ArrayList<>();
-       ArrayList<Avion> listaAviones=new ArrayList<>();
-       
-       System.out.println("++++++++++++++++++++++++++++++++++++++++");
-       System.out.println("BIENVENIDO AL SISTEMA");
-       System.out.println("++++++++++++++++++++++++++++++++++++++++");
-       Scanner sc=new Scanner(System.in);
-       System.out.print("USUARIO: ");
-       String usuario=sc.nextLine();
-       System.out.print("CONTRASEÑA: ");
-       String contrasena=sc.nextLine();
-       
-       
+    public static void main(String[] args){
+        listaUsuarios=new ArrayList<>();
+        listaReservas=new ArrayList<>();
+        listaItinerarios=new ArrayList<>();
+        listaAviones=new ArrayList<>();
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("BIENVENIDO AL SISTEMA");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("USUARIO: ");
+        String usuario=sc.nextLine();
+        System.out.print("CONTRASEÑA: ");
+        String contrasena=sc.nextLine();
+        System.out.println("1. Comprar tickets aeres");
+        System.out.println("2. Consultar reservas");
+        System.out.println("3. Salir");
+        int opc=0;
+        while(opc!=3){
+            System.out.println("Ingrese opcion: ");
+            opc=sc.nextInt();
+            sc.nextLine();
+            switch(opc){
+                case 1:
+                   
+                   break;
+                case 2:
+                    
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+                    break;
+
+            }
+        }
    }
    
 }
