59d9c457d725365ca7d3f377c7120eeb182d9268
ZarateSofia <DELL@DESKTOP-N9P7D59>
2022-11-27 15:21:21 -0500

    actualizado

--- Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Cliente.java
+++ Proyecto1P-Chaguay-Cruz-Zarate/src/Sistema/Cliente.java
@@ -181,7 +181,8 @@ public class Cliente extends Usuario {
                 System.out.println("\n******PASO 2*****\n*****************");
                 System.out.println("-----------ASIENTOS----------");
 
-                System.out.println("Para tu vuelo de ida " + vueloIda.getCodigoVuelo() + " se te ha asignado el asiento: " +Reserva.AsignarAsientos() );
+                System.out.println("Para tu vuelo de ida " + vueloIda.getCodigoVuelo() + " se te ha asignado el asiento: ");
+                
                 System.out.println("Para tu vuelo de retorno " + vueloRetorno.getCodigoVuelo() + " se te ha asignado el asiento: " );
                 //CREACION DE VUELO RESERVA
                 //CREACION DE TXT DE VUELO RESERVA
